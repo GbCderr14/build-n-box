9f8df453878d8b68efdeb8776819c17ee755744a
Murtadha <mnisyif@gmail.com>
2022-08-21 00:18:30 -0400

    Working with Header.css

--- src/components/main/landingPage/LandingPage.js
+++ src/components/main/landingPage/LandingPage.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./LandingPage.css";
 
 export default function LandingPage() {
 	return (
-		<div>
-			<h3> Out-source the best of the best</h3>
-			<h1>Build your Dream PC with Ease</h1>
+		<div className="landing_page">
+			<div> Out-source the best of the best</div>
+			<div>Build your Dream PC with Ease</div>
 		</div>
 	);
 }
