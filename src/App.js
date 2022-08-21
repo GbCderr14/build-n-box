fb257c905f04084e6ca52abdfec5aaf534542103
Murtadha <mnisyif@gmail.com>
2022-08-21 00:26:24 -0400

    Remove large video file and update current directory files

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import "./App.css";
 import Header from "./components/header/Header";
 import LandingPage from "./components/main/landingPage/LandingPage";
 import Footer from "./components/footer/Footer";
+import ShopCategory from "./components/main/shopCategory/ShopCategory";
 
 function App() {
 	return (
 		<div className="container-1">
-			<Header />
-			<LandingPage />
+			<div className="hero">
+				<video className="video" autoPlay loop muted>
+					<source src="/LandingPageVid.mp4" type="video/mp4" />
+				</video>
+				<div className="hero_content">
+					<Header />
+					<LandingPage />
+				</div>
+			</div>
+			<ShopCategory />
 			<Footer />
 		</div>
 	);
---
+++ src/components/main/shopCategory/ShopCategory.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShopCategory = () => {
+	return <div className="category_section"></div>;
+};
+
+export default ShopCategory;
