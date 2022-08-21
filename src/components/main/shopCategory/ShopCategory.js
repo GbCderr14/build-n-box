3a8d34a2d04a3b68535bb5764846a80f2b384a5b
Murtadha <mnisyif@gmail.com>
2022-08-21 00:19:21 -0400

    Add LandingPage.css and create new component, shopCategory

---
+++ src/components/main/shopCategory/ShopCategory.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ShopCategory = () => {
+	return (
+		<div className="category_section">
+			<div className="card"></div>
+			<img className="card_img" alt="some_pic" />
+			<div>Some text here</div>
+		</div>
+	);
+};
+
+export default ShopCategory;
