de4a0b518c7a7c74c3b73e40fb72b2e0b6bffb86
Murtadha <mnisyif@gmail.com>
2022-08-22 13:48:36 -0400

    Modify ShopCategory.js component

--- src/components/main/shopCategory/ShopCategory.js
+++ src/components/main/shopCategory/ShopCategory.js
@@ -1,15 +1,14 @@
 import React from "react";
 
 const ShopCategory = () => {
-
 	return (
 		<div className="category_section">
-			<div className="card"></div>
-			<img className="card_img" alt="some_pic" />
-			<div>Some text here</div>
+			<div className="card">
+				<img className="card_img" alt="some_pic" />
+				<div>Some text here</div>
+			</div>
 		</div>
 	);
-
 };
 
 export default ShopCategory;
